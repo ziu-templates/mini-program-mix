40721466771870cf1573d15af3d539cddf5cad0c
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-05-16 09:30:16 +0800

    feat: 增加eslint config

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -110,5 +110,9 @@ module.exports = {
     "generator-star-spacing": 0,
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    "no-warning-comments": process.env.TODO ? [2, {
+      terms: ["todo", "fixme", "xxx", "mock", "test"],
+      location: "start",
+    }] : 0,
   },
 };
