98dc8e50dea0c91bb797b19ba907fe7cddbaacfe
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-05-16 15:41:57 +0800

    feat: 增加卡点

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -111,7 +111,7 @@ module.exports = {
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
     "no-warning-comments": process.env.TODO ? [2, {
-      terms: ["todo", "fixme", "xxx", "mock", "test"],
+      terms: ["todo", "fixme", "xxx", "mock", "test", "review"],
       location: "start",
     }] : 0,
   },
